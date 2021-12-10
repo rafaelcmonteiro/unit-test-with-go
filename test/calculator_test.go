be8ac341598d1ef9a20b9088f7781b98e3ce23ed
rafael cardoso <rafael.monteiro@semantix.com.br>
2021-12-10 16:38:57 -0300

    gg

--- test/calculator_test.go
+++ test/calculator_test.go
@@ -17,17 +17,17 @@ var itsTestCase []testCases
 
 func TestCalculateIsArmstrong(t *testing.T) {
 	itsTestCase = []testCases{
-		testCases{
+		{
 			value:    371,
 			expected: true,
 		},
-		testCases{
+		{
 			value:    370,
 			expected: true,
 		},
 	}
 	for _, testCase := range itsTestCase {
-		response := "Should work for number " + strconv.Itoa(testCase.value)
+		response := "Should work for case " + strconv.Itoa(testCase.value)
 		t.Run(response, func(t *testing.T) {
 			testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
 			if testCase.actual != testCase.expected {
@@ -38,26 +38,25 @@ func TestCalculateIsArmstrong(t *testing.T) {
 	}
 }
 
-// func TestNegativeCalculateIsArmstrong(t *testing.T) {
-// 	t.Run("Should fail for case 350", func(t *testing.T) {
-// 		testCase := TestCase{
-// 			value:    350,
-// 			expected: false,
-// 		}
-// 		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-// 		if testCase.actual != testCase.expected {
-// 			t.Fail()
-// 		}
-// 	})
+func TestNegativeCalculateIsArmstrong(t *testing.T) {
+	itsTestCase = []testCases{
+		{
+			value:    350,
+			expected: false,
+		},
+		{
+			value:    300,
+			expected: false,
+		},
+	}
+	for _, testCase := range itsTestCase {
+		response := "Should fail for case " + strconv.Itoa(testCase.value)
+		t.Run(response, func(t *testing.T) {
+			testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+			if testCase.actual != testCase.expected {
+				t.Fail()
+			}
+		})
 
-// 	t.Run("Should fail for case 300", func(t *testing.T) {
-// 		testCase := TestCase{
-// 			value:    300,
-// 			expected: false,
-// 		}
-// 		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-// 		if testCase.actual != testCase.expected {
-// 			t.Fail()
-// 		}
-// 	})
-// }
+	}
+}
